a3a1126e0ec68559d28e63b9b37e0d3936bf2cb2
Rafael <rafa.shiga@gmail.com>
2020-10-07 19:01:48 -0300

    chore: add ga config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import Routes from './routes'
 import { ThemeProvider } from 'styled-components'
 import 'aos/dist/aos.css'
@@ -8,6 +9,9 @@ import theme from './styles/theme'
 import Footer from './components/Footer'
 
 const App: React.FC = () => {
+  ReactGA.initialize('UA-149483264-4')
+  ReactGA.pageview(window.location.pathname + window.location.search)
+
   return (
     <ThemeProvider theme={theme}>
       <Routes />
